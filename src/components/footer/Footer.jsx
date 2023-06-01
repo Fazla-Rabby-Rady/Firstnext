0202d1549d8ac1b2f62bda6c41725e3fca83a61a
Fazla-Rabby-Rady <fazlerabbiridoy@gmail.com>
2023-06-02 01:25:20 +0600

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    let n = new Date().getFullYear();
+  return (
+    <>
+      <div className="">
+        <div className="">©{n} Rady. All rights reserved.</div>
+        <div className=""></div>
+      </div>
+    </>
+  );
+}
+
+export default Footer
